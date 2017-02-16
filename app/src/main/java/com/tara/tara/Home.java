36c2673de9dcf4a74214af0cd56dca1e0fc8c835
timesplice <timesplicemobile@gmail.com>
2017-02-16 19:51:13 +0530

    first push for try

--- app/src/main/java/com/tara/tara/Home.java
+++ app/src/main/java/com/tara/tara/Home.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.facebook.FacebookSdk;
 
 public class Home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
